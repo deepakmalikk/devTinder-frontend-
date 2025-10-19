db89194cffa9421b1006eea101d0311b98df2529
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-19 18:05:00 +0530

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-         <NavBar/>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/login" element={<div>LoginPage</div>} />
+      </Routes>
+    </BrowserRouter>
+      <NavBar />
          </>
   );
 }
