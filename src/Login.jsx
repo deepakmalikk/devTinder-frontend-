699df120fcdbdcebb5602dd38cfef5ae23950d90
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-19 20:18:23 +0530

    Added child routes with Outlet

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      
+  <button className="btn btn-error">Login</button>
+    </div>
+   
+  
+  )
+}
+
+export default Login
