e168241a1954bed1b92967acdb5afa07fe00f2b2
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2025-10-25 12:59:27 +0530

    fixed import bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
-import Profile from "./Profile";
+import Profile from "./components/Profile";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
 import Feed from "./components/Feed";
